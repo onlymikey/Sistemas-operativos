8d97f5e2c8210bbb8cbf3dcb582423202592e5dc
iSauLX <yanezsalazarsaul@gmail.com>
2025-02-05 20:30:56 -0800

    configurar proyecto inicial con Vite, TypeScript y Tailwind CSS; agregar componentes básicos y configuración de ESLint

---
+++ Primera tarea /src/components/Navbar/index.tsx
@@ -0,0 +1,109 @@
+import { Navbar, NavbarContent, NavbarItem, Switch, Button, Popover, PopoverContent, 
+    PopoverTrigger, Form, SelectItem, Select, Input 
+} from "@heroui/react";
+import { useTheme } from "@heroui/use-theme";
+import { FaPlay as Play, FaCloudMoon as Moon, FaPlus as Plus, FaMinus as Minus, FaDivide as Divide, FaPercentage as Percentage} from "react-icons/fa";
+import { IoClose as Cross } from "react-icons/io5";
+import { CiSun as Sun } from "react-icons/ci";
+import { ChangeEvent, FormEvent } from "react";
+import { DataProvider } from "../../providers/DataProvider";
+import { useContext, useState, useEffect } from "react";
+
+
+export default function NavBar(): JSX.Element{
+    const {theme, setTheme}: {theme: string, setTheme: (value: string) => void} = useTheme();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [currentOp, setCurrentOperation] = useState<number>(0); 
+    const [currentValue, setCurrentValue] = useState<number>(0);
+    const {data, setData, isRunning, setIsRunning} = useContext(DataProvider);
+    const [currentTime, setCurrentTime] = useState<number>(0);
+    const [time, setTime] = useState<number>(1);
+
+
+    useEffect(() => {
+    let interval: any; 
+    
+    if (isRunning) {
+      interval = setInterval(() => {
+        setCurrentTime((prev: number) => prev + 1);
+      }, 1000);
+    }
+  
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isRunning]); // Solo dependemos de isRunning
+
+
+    function handleSumbit(event: FormEvent<HTMLFormElement>){
+        const formData = Object.fromEntries(new FormData(event.currentTarget));
+        event.preventDefault();
+        setData([...data, formData]);
+        setIsOpen(false); 
+        setCurrentOperation(0);
+    }
+
+    return (
+        <Navbar isBordered >
+            <NavbarContent justify="start">
+                <NavbarItem>
+                    <Button startContent={<Play />} variant='flat' color='secondary' isDisabled={data.length === 0}
+                    onPress={() => setIsRunning((prev: boolean) => !prev)}>
+                        Iniciar
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="center">
+                <NavbarItem>
+                    <Popover showArrow backdrop="opaque" isOpen={isOpen} onOpenChange={setIsOpen}>
+                        <PopoverTrigger>
+                            <Button variant="flat" color="primary" startContent={<Plus />}>
+                                Nuevo proceso. 
+                            </Button>
+                        </PopoverTrigger>
+                    <PopoverContent>
+                        <Form className="my-2" validationBehavior="native" onSubmit={handleSumbit}>
+                            <Input label="Primer numero" type="number" variant="bordered" placeholder="Primer numero" 
+                            name="firstNumber" isRequired />
+                            <Select label="Operacion" placeholder="Operacion a realizar" isRequired 
+                            onChange={(event: ChangeEvent<HTMLSelectElement>) => setCurrentOperation(parseInt(event.target.value))}
+                            variant="bordered" name="operation">
+                                <SelectItem value="1" key={1} startContent={<Plus/>}>Suma</SelectItem>
+                                <SelectItem value="2" key={2} startContent={<Minus/>}>Resta</SelectItem>
+                                <SelectItem value="3" key={3} startContent={<Cross/>}>Multiplicacion</SelectItem>
+                                <SelectItem value="4" key={4} startContent={<Divide/>}>Division</SelectItem>
+                                <SelectItem value="5" key={5} startContent={<Percentage />}>Modulo</SelectItem>
+                            </Select>
+                            <Input label="Segundo numero" type="number" variant="bordered" placeholder="Segundo numero" 
+                            name="secondNumber" onChange={(event: ChangeEvent<HTMLInputElement>) => setCurrentValue(parseInt(event.target.value))}
+                            isInvalid={(currentValue === 0 && (currentOp === 4 || currentOp === 5)) || isNaN(currentValue)}
+                            errorMessage={isNaN(currentValue) ? "El valor debe ser un numero" : "El valor no puede ser 0"}
+                            isRequired />
+                            <Input label="Nombre del programador" type="text" variant="bordered" placeholder="Nombre" 
+                            name="name"
+                            isRequired />
+                            <Input label="Tiempo" placeholder="Tiempo de ejecucion" type="number" variant="bordered"
+                            name="time" isRequired
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => setTime(parseInt(event.target.value))}
+                            isInvalid={time <= 0 || isNaN(time)}
+                            errorMessage={isNaN(time) ? "El valor debe ser un numero" : "El tiempo debe ser mayor a 0"}
+                            />
+                            <Button type="submit" variant="flat" color="primary" className='w-full'>Crear nuevo proceso</Button>
+                        </Form>
+                    </PopoverContent>
+                    </Popover>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem>
+                    <Switch startContent={<Moon />} endContent={<Sun />} size="lg" className="hidden md:block"
+                     onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+                     isSelected={theme === "dark"} aria-label="Cambiar el tema de la aplicación" />
+                </NavbarItem>
+                <NavbarItem>
+                    <Input isReadOnly value={currentTime.toString()} label="Tiempo transcurrido" />
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    )
+}
---
+++ Primera tarea /src/components/Process/index.tsx
@@ -0,0 +1,108 @@
+import { Card, CardBody, Chip } from "@heroui/react";
+import { motion } from "framer-motion";
+import type { Variants } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { DataProvider } from "../../providers/DataProvider";
+type ProcessProps = {
+    firstNumber: string;
+    secondNumber: string;
+    operation: number;
+    id: number;
+    name: string;
+    time: string;
+    isRunning?: boolean;
+    isDone?: boolean;
+    uniqueId?: number;  
+}
+
+export default function Process({ firstNumber, secondNumber, operation, id, name, time, isRunning = false, isDone = false, uniqueId }: ProcessProps): JSX.Element {
+    const [currentTime, setCurrentTime] = useState<number>(parseInt(time));
+    const { setCurrentValue, setDone, setCurrentOperation } = useContext(DataProvider);
+    const variants: Variants = {
+        initial: {
+            scale: 0.8,
+            opacity: 0
+        },
+        animate: {
+            scale: 1,
+            opacity: 1
+        },
+        exit: {
+            scale: 0.5,
+            opacity: 0
+        }
+    }
+    useEffect(() => {
+        if (isRunning) {
+            if (currentTime <= 0) {
+                setCurrentOperation((prev: any[]) => {
+                    const updatedProcesses = prev.map((process: any[]) => process.filter((item: any) => item.uniqueId !== uniqueId));
+                    return updatedProcesses.filter((process: any[]) => process.length > 0);
+                });
+
+                setDone((prev: any[]) => [
+                    ...prev,
+                    { firstNumber, secondNumber, operation, id, name, time },
+                ]);
+                setCurrentValue((prev: number) => prev + 1);
+            } else {
+                const timeout = setTimeout(() => {
+                    setCurrentTime((prev: number) => prev - 1);
+                }, 1000);
+                return () => {
+                    clearTimeout(timeout);
+                };
+            }
+        }
+    }, [isRunning, currentTime, firstNumber, secondNumber, operation, id, name, time, uniqueId]);
+
+
+    function getOperation(value: number): string {
+        switch (value) {
+            case 1:
+                return "+";
+            case 2:
+                return "-";
+            case 3:
+                return "*";
+            case 4:
+                return "÷";
+            case 5:
+                return "%";
+            default:
+                throw new Error("Invalid operation");
+        }
+    }
+
+    function getResult(firstNumber: string, secondNumber: string, operation: number): number {
+        switch (operation) {
+            case 1:
+                return parseInt(firstNumber) + parseInt(secondNumber);
+            case 2:
+                return parseInt(firstNumber) - parseInt(secondNumber);
+            case 3:
+                return parseInt(firstNumber) * parseInt(secondNumber);
+            case 4:
+                return Math.round(parseInt(firstNumber) / parseInt(secondNumber));
+            case 5:
+                return parseInt(firstNumber) % parseInt(secondNumber);
+            default:
+                throw new Error("Invalid operation");
+        }
+    }
+    return (
+        <motion.div variants={variants} initial="initial" animate="animate" exit="exit" className="mb-3">
+            <Card className={isRunning ? "bg-red-600/20 border-red-600 border-1 border-dashed" : 'bg-sky-500/20 border-sky-500 border-1 border-dashed'}>
+                <CardBody className='flex flex-col items-center justify-start'>
+                    <h3 className='font-inter font-semibold text-start w-full'>Proceso #{id}</h3>
+                    <p className='font-inter text-3xl font-extrabold'>{firstNumber} {getOperation(operation)} {secondNumber} {isDone && "= " + getResult(firstNumber, secondNumber, operation)}</p>
+                    <section className='w-full flex items-center justify-between flex-row'>
+                        <p className="font-inter text-semibold">Tiempo: {currentTime}s</p>
+                        <Chip color="primary" size="sm">{name}</Chip>
+                    </section>
+                </CardBody>
+            </Card>
+        </motion.div>
+    )
+}
