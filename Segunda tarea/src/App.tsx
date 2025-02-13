377d3fb39c135539ca863b0e4af90be5a87f651d
iSauLX <yanezsalazarsaul@gmail.com>
2025-02-12 16:18:01 -0800

    mejorar gestión de procesos: agregar manejo de eventos de teclado en App y ajustar lógica de renderizado en componentes Batch y Process

--- Segunda tarea/src/App.tsx
+++ Segunda tarea/src/App.tsx
@@ -1,12 +1,9 @@
-import Section from "./components/Section"
-import NavBar from "./components/Navbar"
-import Batch from "./components/Batch"
-import Process from "./components/Process"
-import { useEffect, useState, useCallback } from "react"
-import { DataProvider } from "./providers/DataProvider"
-
-
-
+import Section from "./components/Section";
+import NavBar from "./components/Navbar";
+import Batch from "./components/Batch";
+import Process from "./components/Process";
+import { useEffect, useState, useCallback } from "react";
+import { DataProvider } from "./providers/DataProvider";
 
 export default function App() {
   const [data, setData] = useState<any[]>([]);
@@ -28,51 +25,97 @@ export default function App() {
       // data ahora es un array plano de procesos
       // Extraer el primer valor de data, y lo eliminamos del array
       if (currentOperation.length === 0) {
-      const [firstValue, ...rest] = splitArray(data, 5);
-      setData(rest.flat());
-      if (!firstValue) return;
-      const processesWithIds = splitArray(firstValue.map((process: any, index: number) => {
-        return {
-          ...process,
-          uniqueId: `${process.operation}-${index}-${Date.now()}`
-        }
-      }), 5)
-      setCurrentOperation((prev) => [...prev, ...processesWithIds]);
-    }
+        const [firstValue, ...rest] = splitArray(data, 5);
+        setData(rest.flat());
+        if (!firstValue) return;
+        const processesWithIds = splitArray(
+          firstValue.map((process: any, index: number) => {
+            return {
+              ...process,
+              uniqueId: `${process.operation}-${index}-${Date.now()}`,
+            };
+          }),
+          5
+        );
+        setCurrentOperation((prev) => [...prev, ...processesWithIds]);
+      }
     }
   }, [isRunning, data, currentOperation]);
-// Modificar este efecto en App.tsx
-useEffect(() => {
-  if ((done.length > 0) && (currentOperation.length === 0) && data.length === 0) {
-    setIsRunning(false);
-  }
-}, [currentOperation, done]);
+  // Modificar este efecto en App.tsx
+  useEffect(() => {
+    if (done.length > 0 && currentOperation.length === 0 && data.length === 0) {
+      setIsRunning(false);
+    }
+  }, [currentOperation, done]);
+
+  useEffect(() => {
+    const hanldeKeyboardEVent = (event: KeyboardEvent) => {
+      if (event.key === "p") {
+        setIsRunning(false);
+      }
+      if (event.key === "c") {
+        setIsRunning(true);
+      }
+    };
+
+    window.addEventListener("keydown", hanldeKeyboardEVent);
+    return () => {
+      window.removeEventListener("keydown", hanldeKeyboardEVent);
+    };
+  }, []);
 
   return (
     <main className="bg-background text-inherit w-full h-screen flex items-center flex-col justify-start">
-
-      <DataProvider.Provider value={{ data, setData, isRunning, setIsRunning, setDone, setCurrentOperation, currentOperation, done, setTime }}>
+      <DataProvider.Provider
+        value={{
+          data,
+          setData,
+          isRunning,
+          setIsRunning,
+          setDone,
+          setCurrentOperation,
+          currentOperation,
+          done,
+          setTime,
+        }}
+      >
         <NavBar time={time} />
         <section className="w-full md:w-3/4 grid grid-cols-1 md:grid-cols-3 gap-4 p-2 ">
           <Section title="Procesos inactivos">
             {splitArray(data, 5).map((batch: any, index: number) => (
-              <Batch key={index} index={index + 1 + Math.floor(done.length / 5) + Math.ceil(currentOperation.flat().length / 5)}>
+              <Batch
+                key={index}
+                index={
+                  index +
+                  1 +
+                  Math.floor(done.length / 5) +
+                  Math.ceil(currentOperation.flat().length / 5)
+                }
+              >
                 {batch.map((process: any, index: number) => (
-                  <Process key={index} id={index + 1} {...process} operation={parseInt(process.operation)} />
+                  <Process
+                    key={index}
+                    id={index + 1}
+                    {...process}
+                    operation={parseInt(process.operation)}
+                  />
                 ))}
               </Batch>
             ))}
           </Section>
           <Section title="Procesos activos">
             {currentOperation.map((batch: any, indexFather: number) => (
-              <Batch key={indexFather} index={indexFather + 1 + Math.floor(done.length / 5)}>
+              <Batch
+                key={indexFather}
+                index={indexFather + 1 + Math.floor(done.length / 5)}
+              >
                 {batch.map((process: any, index: number) => (
                   <Process
                     key={process.uniqueId} // Key única estable
                     uniqueId={process.uniqueId} // Pasamos el uniqueId
                     {...process}
                     operation={parseInt(process.operation)}
-                    isRunning={index === 0 && indexFather === 0}
+                    isRunning={index === 0 && isRunning}
                   />
                 ))}
               </Batch>
@@ -80,7 +123,7 @@ useEffect(() => {
           </Section>
           <Section title="Procesos finalizados">
             {splitArray(done, 5).map((batch: any[], index: number) => (
-              <Batch key={index} index={index +1}>
+              <Batch key={index} index={index + 1}>
                 {batch.map((process: any, index: number) => (
                   <Process
                     key={process.uniqueId || index}
@@ -95,5 +138,5 @@ useEffect(() => {
         </section>
       </DataProvider.Provider>
     </main>
-  )
+  );
 }
--- Segunda tarea/src/components/Process/index.tsx
+++ Segunda tarea/src/components/Process/index.tsx
@@ -5,131 +5,164 @@ import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { DataProvider } from "../../providers/DataProvider";
 type ProcessProps = {
-    firstNumber: string;
-    secondNumber: string;
-    operation: number;
-    id: number;
-    name: string;
-    time: string;
-    isRunning?: boolean;
-    isDone?: boolean;
-    uniqueId?: number;  
-    isErrored?: boolean; 
-}
+  firstNumber: string;
+  secondNumber: string;
+  operation: number;
+  id: number;
+  name: string;
+  time: string;
+  isRunning?: boolean;
+  isDone?: boolean;
+  uniqueId?: number;
+  isErrored?: boolean;
+};
 
-export default function Process({ firstNumber, secondNumber, operation, id, time, isRunning = false, isDone = false, uniqueId, isErrored = false }: ProcessProps): JSX.Element {
-    const [currentTime, setCurrentTime] = useState<number>(parseInt(time));
-    const { setDone, setCurrentOperation, setTime } = useContext(DataProvider);
-    const variants: Variants = {
-        initial: {
-            scale: 0.8,
-            opacity: 0
-        },
-        animate: {
-            scale: 1,
-            opacity: 1
-        },
-        exit: {
-            scale: 0.5,
-            opacity: 0
-        }
+export default function Process({
+  firstNumber,
+  secondNumber,
+  operation,
+  id,
+  time,
+  isRunning = false,
+  isDone = false,
+  uniqueId,
+  isErrored = false,
+}: ProcessProps): JSX.Element {
+  const [currentTime, setCurrentTime] = useState<number>(parseInt(time));
+  const { setDone, setCurrentOperation, setTime } = useContext(DataProvider);
+  const variants: Variants = {
+    initial: {
+      scale: 0.8,
+      opacity: 0,
+    },
+    animate: {
+      scale: 1,
+      opacity: 1,
+    },
+    exit: {
+      scale: 0.5,
+      opacity: 0,
+    },
+  };
+  useEffect(() => {
+    if (isRunning) {
+      if (currentTime <= 0) {
+        setCurrentOperation((prev: any[]) => {
+          const updatedProcesses = prev.map((process: any[]) =>
+            process.filter((item: any) => item.uniqueId !== uniqueId)
+          );
+          return updatedProcesses.filter(
+            (process: any[]) => process.length > 0
+          );
+        });
+
+        setDone((prev: any[]) => [
+          ...prev,
+          { firstNumber, secondNumber, operation, id, time },
+        ]);
+      } else {
+        const timeout = setTimeout(() => {
+          setCurrentTime((prev: number) => prev - 1);
+          setTime((prev: number) => prev + 1);
+        }, 1000);
+        return () => {
+          clearTimeout(timeout);
+        };
+      }
     }
-    useEffect(() => {
-        if (isRunning) {
-            if (currentTime <= 0) {
-                setCurrentOperation((prev: any[]) => {
-                    const updatedProcesses = prev.map((process: any[]) => process.filter((item: any) => item.uniqueId !== uniqueId));
-                    return updatedProcesses.filter((process: any[]) => process.length > 0);
-                });
+  }, [isRunning, currentTime]);
 
-                setDone((prev: any[]) => [
-                    ...prev,
-                    { firstNumber, secondNumber, operation, id, time },
-                ]);
-            } else {
-                const timeout = setTimeout(() => {
-                    setCurrentTime((prev: number) => prev - 1);
-                    setTime((prev: number) => prev + 1);
-                }, 1000);
-                return () => {
-                    clearTimeout(timeout);
-                };
-            }
-        }
-    }, [isRunning, currentTime]);
+  useEffect(() => {
+    const handleKeyboardEvent = (event: KeyboardEvent) => {
+      if (event.key === "i" && isRunning) {
+        setCurrentOperation((prev: any[]) => {
+          const flatArray: any[] = prev.flat();
+          const constFirstElement = flatArray.shift();
+          return [[...flatArray, constFirstElement]];
+        });
+      }
+      if (event.key === "e" && isRunning) {
+        setDone((prev: any[]) => [
+          ...prev,
+          { firstNumber, secondNumber, operation, id, time, isErrored: true },
+        ]);
+        setCurrentOperation((prev: any[]) => {
+          const flatArray: any[] = prev.flat();
+          const updatedArray = flatArray.filter((item: any) => item.id !== id);
+          return updatedArray.length > 0 ? [updatedArray] : [];
+        });
+      }
+    };
+    window.addEventListener("keydown", handleKeyboardEvent);
+    return (): void => {
+      window.removeEventListener("keydown", handleKeyboardEvent);
+    };
+  });
 
-    useEffect(() => {
-        const handleKeyboardEvent = (event: KeyboardEvent) => {
-            if (event.key === "i" && isRunning){
-                setCurrentOperation((prev: any[]) => {
-                    const flatArray: any[] = prev.flat(); 
-                    const constFirstElement = flatArray.shift(); 
-                    return [[...flatArray, constFirstElement]];
-                })
-            }
-            if (event.key === "e" && isRunning){
-                setDone((prev: any[]) => [...prev, {firstNumber, secondNumber, operation, id, time, isErrored: true}]);
-                setCurrentOperation((prev: any[]) => {
-                    const flatArray: any[] = prev.flat(); 
-                    const updatedArray = flatArray.filter((item: any) => item.id !== id); 
-                    return updatedArray.length > 0 ? [updatedArray] : [];
-                })
-            }
-        }
-        window.addEventListener('keydown', handleKeyboardEvent);
-        return (): void => {
-            window.removeEventListener('keydown', handleKeyboardEvent);
-        }
-
-    })
-
-
-    function getOperation(value: number): string {
-        switch (value) {
-            case 1:
-                return "+";
-            case 2:
-                return "-";
-            case 3:
-                return "*";
-            case 4:
-                return "÷";
-            case 5:
-                return "%";
-            default:
-                throw new Error("Invalid operation");
-        }
+  function getOperation(value: number): string {
+    switch (value) {
+      case 1:
+        return "+";
+      case 2:
+        return "-";
+      case 3:
+        return "*";
+      case 4:
+        return "÷";
+      case 5:
+        return "%";
+      default:
+        throw new Error("Invalid operation");
     }
+  }
 
-    function getResult(firstNumber: string, secondNumber: string, operation: number): number {
-        switch (operation) {
-            case 1:
-                return parseInt(firstNumber) + parseInt(secondNumber);
-            case 2:
-                return parseInt(firstNumber) - parseInt(secondNumber);
-            case 3:
-                return parseInt(firstNumber) * parseInt(secondNumber);
-            case 4:
-                return Math.round(parseInt(firstNumber) / parseInt(secondNumber));
-            case 5:
-                return parseInt(firstNumber) % parseInt(secondNumber);
-            default:
-                throw new Error("Invalid operation");
-        }
+  function getResult(
+    firstNumber: string,
+    secondNumber: string,
+    operation: number
+  ): number {
+    switch (operation) {
+      case 1:
+        return parseInt(firstNumber) + parseInt(secondNumber);
+      case 2:
+        return parseInt(firstNumber) - parseInt(secondNumber);
+      case 3:
+        return parseInt(firstNumber) * parseInt(secondNumber);
+      case 4:
+        return Math.round(parseInt(firstNumber) / parseInt(secondNumber));
+      case 5:
+        return parseInt(firstNumber) % parseInt(secondNumber);
+      default:
+        throw new Error("Invalid operation");
     }
-    return (
-        <motion.div variants={variants} initial="initial" animate="animate" exit="exit" className="mb-3">
-            <Card className='bg-sky-500/20 border-sky-500 border-1 border-dashed data-[status="running"]:bg-green-600/20 data-[status="running"]:border-green-600 data-[status="error"]:bg-red-600/20 data-[status="error"]:border-red-600' data-status={isRunning ? "running" : isErrored ? "error" : "done"}>
-                <CardBody className='flex flex-col items-center justify-start'>
-                    <div>
-                        <h3 className='font-inter font-semibold text-start w-full'>Proceso #{id}</h3>
-                    </div>
-                    <p className='font-inter text-3xl font-extrabold'>{firstNumber} {getOperation(operation)} {secondNumber} {isDone && "= " + getResult(firstNumber, secondNumber, operation)}</p>
-                    <section className='w-full flex items-center justify-between flex-row'>
-                        <p className="font-inter text-semibold">Tiempo: {currentTime}s</p>
-                    </section>
-                </CardBody>
-            </Card>
-        </motion.div>
-    )
+  }
+  return (
+    <motion.div
+      variants={variants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className="mb-3"
+    >
+      <Card
+        className='bg-sky-500/20 border-sky-500 border-1 border-dashed data-[status="running"]:bg-green-600/20 data-[status="running"]:border-green-600 data-[status="error"]:bg-red-600/20 data-[status="error"]:border-red-600'
+        data-status={isRunning ? "running" : isErrored ? "error" : "done"}
+      >
+        <CardBody className="flex flex-col items-center justify-start">
+          <div>
+            <h3 className="font-inter font-semibold text-start w-full">
+              Proceso #{id}
+            </h3>
+          </div>
+          <p className="font-inter text-3xl font-extrabold">
+            {firstNumber} {getOperation(operation)} {secondNumber}{" "}
+            {isDone && "= " + getResult(firstNumber, secondNumber, operation)}
+          </p>
+          <section className="w-full flex items-center justify-between flex-row">
+            <p className="font-inter text-semibold">Tiempo: {currentTime}s</p>
+          </section>
+        </CardBody>
+      </Card>
+    </motion.div>
+  );
 }
