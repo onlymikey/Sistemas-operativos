9302b3f48e5cc9cb9f72e8e911776efb711d5056
Saul Salazar <yanezsalazarsaul@gmail.com>
2025-04-08 00:20:41 -0700

    agregar tipos de datos para procesos y memoria, y crear componentes de lista y texto animado en EightHomework

---
+++ sistemas-operativos/src/pages/EightHomework/types/types.d.ts
@@ -0,0 +1,23 @@
+export type ProcessType = {
+    id: number; 
+    firstNumber: number; 
+    secondNumber: number;
+    operation: number; 
+    time: number; 
+    timeLeft?: number; 
+    quantumTime?: number; 
+    status: "Nuevo" | "Listo" | "Ejecutando" | "Terminado" | "Error" | "Bloqueado"; 
+    startTime?: number; 
+    endTime?: number; 
+    responseTime?: number; 
+    returnTime?: number; 
+    waitTime?: number;
+    onSave?: boolean; 
+    memorySize: number; 
+}
+
+export type MemoryType = {
+    id: number;
+    occupied: number;
+    process: number | null;
+}
